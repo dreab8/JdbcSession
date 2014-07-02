a5a20091fefef1532709324b2f62eb0d0a9e1a40
Andrea Boriero <andrea@hibernate.org>
2014-07-02 16:33:31 +0100

    Added QueryPreparedStatementBuilder tests

---
+++ src/test/java/org/hibernate/test/resource/jdbc/internal/AbstractQueryPreparedStatementBuilderTest.java
@@ -0,0 +1,74 @@
+package org.hibernate.test.resource.jdbc.internal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.hibernate.resource.jdbc.spi.JdbcSessionContext;
+import org.hibernate.resource.jdbc.spi.QueryPreparedStatementBuilder;
+
+import org.junit.Test;
+
+import org.hibernate.test.resource.jdbc.common.JdbcSessionContextStandardTestingImpl;
+
+import static org.hibernate.resource.jdbc.PreparedStatementQueryOperationSpec.ResultSetConcurrency;
+import static org.hibernate.resource.jdbc.PreparedStatementQueryOperationSpec.ResultSetType;
+import static org.mockito.Mockito.mock;
+
+public abstract class AbstractQueryPreparedStatementBuilderTest {
+
+	private Connection connection = mock( Connection.class );
+	protected QueryPreparedStatementBuilder queryBuilder;
+	private final String sql = "Select * from item";
+	private final JdbcSessionContext context = JdbcSessionContextStandardTestingImpl.INSTANCE;
+	private ConnectionMethodCallCheck verifier;
+
+	protected interface ConnectionMethodCallCheck {
+		void verify(Connection c, String expectedSql) throws SQLException;
+
+		void verify(
+				Connection c,
+				String expectedSql,
+				ResultSetType expectedResultSetType,
+				ResultSetConcurrency expectedResultSetConcurrency) throws SQLException;
+	}
+
+	protected void setMethodCallCheck(ConnectionMethodCallCheck v) {
+		this.verifier = v;
+	}
+
+	@Test
+	public void prepareCallIsInvokedWithoutResultTypeAndResultConcurrenecy() throws SQLException {
+		queryBuilder.buildQueryStatement( connection, sql, context, null, null );
+
+		verifier.verify( connection, sql );
+
+		queryBuilder.buildQueryStatement(
+				connection, sql, context,
+				ResultSetType.FORWARD_ONLY, null
+		);
+
+		verifier.verify( connection, sql );
+
+		queryBuilder.buildQueryStatement(
+				connection, sql, context,
+				null, ResultSetConcurrency.READ_ONLY
+		);
+
+		verifier.verify( connection, sql );
+	}
+
+	@Test
+	public void prepareCallIsInvokedWithTheExpectedResultTypeAndResultConcurrenecy() throws SQLException {
+		ResultSetType expectedResultSetType = ResultSetType.FORWARD_ONLY;
+		ResultSetConcurrency expectedResultSetConcurrency = ResultSetConcurrency.READ_ONLY;
+
+		queryBuilder.buildQueryStatement(
+				connection, sql, context,
+				expectedResultSetType, expectedResultSetConcurrency
+		);
+
+		verifier.verify( connection, sql, expectedResultSetType, expectedResultSetConcurrency );
+
+	}
+
+}
---
+++ src/test/java/org/hibernate/test/resource/jdbc/internal/CallableQueryPreparedStatementBuilderImplTest.java
@@ -0,0 +1,76 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) {DATE}, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.test.resource.jdbc.internal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.mockito.Mockito;
+
+import org.hibernate.resource.jdbc.internal.CallableQueryPreparedStatementBuilderImpl;
+
+import org.junit.Before;
+
+import static org.hibernate.resource.jdbc.PreparedStatementQueryOperationSpec.ResultSetConcurrency;
+import static org.hibernate.resource.jdbc.PreparedStatementQueryOperationSpec.ResultSetType;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+
+
+/**
+ * @author Andrea Boriero
+ */
+public class CallableQueryPreparedStatementBuilderImplTest
+		extends AbstractQueryPreparedStatementBuilderTest {
+
+	@Before
+	public void setUp() throws SQLException {
+		queryBuilder = CallableQueryPreparedStatementBuilderImpl.INSTANCE;
+		setMethodCallCheck(
+				new ConnectionMethodCallCheck() {
+
+					@Override
+					public void verify(Connection c, String expectedSql) throws SQLException {
+						Mockito.verify( c ).prepareCall( expectedSql );
+						reset( c );
+					}
+
+					@Override
+					public void verify(
+							Connection c,
+							String expectedSql,
+							ResultSetType expectedResultSetType,
+							ResultSetConcurrency expectedResultSetConcurrency)
+							throws SQLException {
+						Mockito.verify( c ).prepareCall(
+								expectedSql,
+								expectedResultSetType.getJdbcConstantValue(),
+								expectedResultSetConcurrency.getJdbcConstantValue()
+						);
+
+					}
+				}
+		);
+	}
+}
