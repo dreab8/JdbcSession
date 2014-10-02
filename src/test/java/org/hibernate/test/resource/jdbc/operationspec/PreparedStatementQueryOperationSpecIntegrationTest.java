ae8e4192c465e87684068a861b87f8c81ecc1c9b
Andrea Boriero <andrea@hibernate.org>
2014-10-02 17:17:40 +0100

    Add PreparedStatementQueryOperationSpecIntegrationTest

--- src/test/java/org/hibernate/test/resource/jdbc/operationspec/PreparedStatementQueryOperationSpecIntegrationTest.java
+++ src/test/java/org/hibernate/test/resource/jdbc/operationspec/PreparedStatementQueryOperationSpecIntegrationTest.java
@@ -23,8 +23,216 @@
  */
 package org.hibernate.test.resource.jdbc.operationspec;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.hibernate.JDBCException;
+import org.hibernate.resource.jdbc.JdbcSession;
+import org.hibernate.resource.jdbc.PreparedStatementQueryOperationSpec;
+import org.hibernate.resource.jdbc.QueryOperationSpec;
+import org.hibernate.resource.jdbc.internal.BatchFactoryImpl;
+import org.hibernate.resource.jdbc.internal.ResourceRegistryStandardImpl;
+import org.hibernate.resource.jdbc.spi.JdbcSessionFactory;
+import org.hibernate.resource.jdbc.spi.ParameterBindings;
+import org.hibernate.resource.jdbc.spi.QueryStatementBuilder;
+import org.hibernate.resource.jdbc.spi.ResultSetProcessor;
+import org.hibernate.resource.jdbc.spi.StatementExecutor;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.hibernate.test.resource.common.DatabaseConnectionInfo;
+import org.hibernate.test.resource.jdbc.common.JdbcSessionOwnerTestingImpl;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
 /**
  * @author Andrea Boriero
  */
 public class PreparedStatementQueryOperationSpecIntegrationTest {
+
+	private static final String BILLING_ADDRESS_INSERT_SQL = "INSERT INTO BillingAddress (ADDRESS_ID, owner) values  (1,'owner')";
+
+	private JdbcSession jdbcSession;
+	private Connection localConnection;
+
+	@Before
+	public void setUp() throws SQLException {
+		localConnection = DatabaseConnectionInfo.INSTANCE.makeConnection();
+		localConnection.setAutoCommit( false );
+		createTables();
+		jdbcSession = createJdbSession();
+	}
+
+	@After
+	public void tearDown() throws SQLException {
+		dropTables();
+		jdbcSession.close();
+	}
+
+	@Test
+	public void testQueryForEntity() throws SQLException {
+		execute( BILLING_ADDRESS_INSERT_SQL );
+
+		PreparedStatementQueryOperationSpec<BillingAddress> operationSpec = new PreparedStatementQueryOperationSpec() {
+			@Override
+			public QueryStatementBuilder<? extends PreparedStatement> getQueryStatementBuilder() {
+				return new SimpleStatementBuilder();
+			}
+
+			@Override
+			public ParameterBindings getParameterBindings() {
+				return new SimpleParameterBindings();
+			}
+
+			@Override
+			public StatementExecutor getStatementExecutor() {
+				return new SimpleStatementExecutor();
+			}
+
+			@Override
+			public ResultSetProcessor getResultSetProcessor() {
+				return new SimpleResultSetProcessor();
+			}
+
+			@Override
+			public int getQueryTimeout() {
+				return 0;
+			}
+
+			@Override
+			public String getSql() {
+				return "Select ADDRESS_ID, owner from  BillingAddress where ADDRESS_ID = ?";
+			}
+
+			@Override
+			public ResultSetType getResultSetType() {
+				return ResultSetType.FORWARD_ONLY;
+			}
+
+			@Override
+			public ResultSetConcurrency getResultSetConcurrency() {
+				return ResultSetConcurrency.READ_ONLY;
+			}
+
+			@Override
+			public int getOffset() {
+				return 0;
+			}
+
+			@Override
+			public int getLimit() {
+				return 0;
+			}
+		};
+
+		try {
+			BillingAddress billingAddress = jdbcSession.accept( operationSpec );
+			localConnection.commit();
+			assertThat( billingAddress.getId(), is( 1L ) );
+			assertThat( billingAddress.getOwner(), is( "owner" ) );
+		}
+		catch (JDBCException e) {
+			localConnection.rollback();
+			throw e;
+		}
+	}
+
+	private class SimpleStatementBuilder implements QueryStatementBuilder {
+
+		@Override
+		public Statement buildQueryStatement(
+				Connection connection,
+				String sql,
+				QueryOperationSpec.ResultSetType resultSetType,
+				QueryOperationSpec.ResultSetConcurrency resultSetConcurrency) throws SQLException {
+			return connection.prepareStatement(
+					sql,
+					resultSetType.getJdbcConstantValue(),
+					resultSetConcurrency.getJdbcConstantValue()
+			);
+		}
+	}
+
+	private class SimpleParameterBindings implements ParameterBindings {
+
+		@Override
+		public void bindParameters(PreparedStatement statement) throws SQLException {
+			statement.setLong( 1, 1 );
+		}
+	}
+
+	private class SimpleStatementExecutor implements StatementExecutor {
+		@Override
+		public ResultSet execute(PreparedStatement statement) throws SQLException {
+			return statement.executeQuery();
+		}
+	}
+
+	private class SimpleResultSetProcessor implements ResultSetProcessor {
+		@Override
+		public Object extractResults(ResultSet resultSet) throws SQLException {
+			resultSet.next();
+			long addressId = resultSet.getLong( "ADDRESS_ID" );
+			String owner = resultSet.getString( "owner" );
+
+			BillingAddress billingAddress = new BillingAddress();
+			billingAddress.setId( addressId );
+			billingAddress.setOwner( owner );
+			return billingAddress;
+		}
+	}
+
+	private JdbcSession createJdbSession() {
+		JdbcSessionOwnerTestingImpl JDBC_SESSION_OWNER = new JdbcSessionOwnerTestingImpl();
+		JDBC_SESSION_OWNER.setBatchFactory( new BatchFactoryImpl( 1 ) );
+		return JdbcSessionFactory.INSTANCE.create( JDBC_SESSION_OWNER, new ResourceRegistryStandardImpl() );
+	}
+
+	private void createTables() throws SQLException {
+		final String createBillingAddressTableSql = "create table BillingAddress (" +
+				"        ADDRESS_ID bigint not null," +
+				"        owner varchar(255)," +
+				"        primary key (ADDRESS_ID) )";
+		execute( createBillingAddressTableSql );
+	}
+
+	private void dropTables() throws SQLException {
+		execute( "DROP table BillingAddress IF EXISTS" );
+	}
+
+	private void execute(String sql) throws SQLException {
+		PreparedStatement statement = localConnection.prepareStatement( sql );
+		statement.execute();
+	}
+
+	public class BillingAddress {
+
+		protected Long id;
+
+		protected String owner;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getOwner() {
+			return owner;
+		}
+
+		public void setOwner(String owner) {
+			this.owner = owner;
+		}
+	}
+
 }
